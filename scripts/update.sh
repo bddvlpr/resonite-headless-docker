e1434a54e970b92717ef56ead9a8386f5fe575f8
Luna Simons <luna@bddvlpr.com>
2026-05-26 17:31:52 +0200

    fix: include discord libs

--- scripts/update.sh
+++ scripts/update.sh
@@ -24,5 +24,11 @@ curl -SsL "https://github.com/resonite-modding-group/ResoniteModLoader/releases/
 echo "Updating and/or validating mods..."
 curl -SsL "https://github.com/New-Project-Final-Final-WIP/HeadlessTweaks/releases/latest/download/HeadlessTweaks.dll" \
   -o "$HEADLESS_DIR/rml_mods/HeadlessTweaks.dll"
+curl -SsL "https://github.com/New-Project-Final-Final-WIP/HeadlessTweaks/releases/latest/download/Discord.Net.Core.dll" \
+  -o "$HEADLESS_DIR/rml_libs/Discord.Net.Core.dll"
+curl -SsL "https://github.com/New-Project-Final-Final-WIP/HeadlessTweaks/releases/latest/download/Discord.Net.Rest.dll" \
+  -o "$HEADLESS_DIR/rml_libs/Discord.Net.Rest.dll"
+curl -SsL "https://github.com/New-Project-Final-Final-WIP/HeadlessTweaks/releases/latest/download/Discord.Net.Webhook.dll" \
+  -o "$HEADLESS_DIR/rml_libs/Discord.Net.Webhook.dll"
 curl -SsL "https://codeberg.org/Raidriar/StresslessHeadless/releases/download/latest/StresslessHeadless.dll" \
   -o "$HEADLESS_DIR/rml_mods/StresslessHeadless.dll"
